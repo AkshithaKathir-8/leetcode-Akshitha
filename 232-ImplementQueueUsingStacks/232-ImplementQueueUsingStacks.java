0794519e51228a561ce1c4196d0100cf1ae59aed
AkshithaKathir-8 <akshithakathir2006@gmail.com>
2026-08-12 08:58:40 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.java
@@ -0,0 +1,29 @@
+// Last updated: 8/12/2026, 8:58:40 AM
+class MyQueue {
+        Stack<Integer> st=new Stack<>();
+        Stack<Integer> helper=new Stack<>();
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+       while(st.size()>0){
+        helper.push(st.pop());
+       }
+       st.push(x);
+       while(helper.size()>0){
+        st.push(helper.pop());
+       }
+    }
+    
+    public int pop() {
+       return st.pop();
+    }
+    
+    public int peek() {
+       return st.peek();
+    }
+     public boolean empty() {
+        return (st.size()==0);
+    }
+}
