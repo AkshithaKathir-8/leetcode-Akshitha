d65d4611df0750d1e76e7e4e2b730507ddd2e038
AkshithaKathir-8 <akshithakathir2006@gmail.com>
2026-08-12 08:57:54 +0530

    Create 299-BullsAndCows

---
+++ 299-BullsAndCows/299-BullsAndCows.java
@@ -0,0 +1,27 @@
+// Last updated: 8/12/2026, 8:57:54 AM
+class Solution {
+    public String getHint(String secret, String guess) {
+        int n = secret.length();
+        Map<Character, Integer> map = new HashMap<>();
+        int bulls = 0;
+        int cows = 0;
+        for(int i=0; i<n; i++) {
+            char s = secret.charAt(i);
+            char g = guess.charAt(i);
+            if(s == g) {
+                bulls++;
+            } else {
+                map.put(s, map.getOrDefault(s, 0) + 1);
+            }
+        }
+        for(int i=0; i<n; i++) {
+            char s = secret.charAt(i);
+            char g = guess.charAt(i);
+            if(s != g && map.getOrDefault(g, 0) > 0) {
+                cows++;
+                map.put(g, map.get(g) - 1);
+            }
+        }
+        return bulls + "A" + cows + "B";
+    }
+} 
