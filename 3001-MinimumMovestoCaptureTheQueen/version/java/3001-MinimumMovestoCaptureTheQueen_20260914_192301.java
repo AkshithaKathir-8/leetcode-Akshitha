ffa9355ccc2790a59d9cbf72d00afb3022ed5589
AkshithaKathir-8 <akshithakathir2006@gmail.com>
2026-09-14 19:23:02 +0530

    Create 3001-MinimumMovestoCaptureTheQueen

---
+++ 3001-MinimumMovestoCaptureTheQueen/version/java/3001-MinimumMovestoCaptureTheQueen_20260914_192301.java
@@ -0,0 +1,10 @@
+// Last updated: 9/14/2026, 7:23:01 PM
+1class Solution {
+2    public int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
+3    if(a == e && !(a == c && d > Math.min(b,f) && d < Math.max(b,f) )) return 1;
+4    if(b == f && !(b == d && c > Math.min(a,e) && c < Math.max(a,e) ))  return 1;
+5    if(c + d == e + f && !(c + d == a + b && a > Math.min(c , e) && a < Math.max(c, e) )) return 1;
+6    if(c-d == e-f && !( c-d == a-b && a > Math.min(c , e) && a < Math.max(c, e) )) return 1;
+7    return 2;
+8}
+9}
